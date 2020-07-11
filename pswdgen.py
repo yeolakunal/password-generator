e28c774ea65979fe641c1f100e6c94d476bd5514
yeolakunal <yeolakunal@gmail.com>
2020-07-12 01:24:14 +0530

    Added GUI using tkinter

--- pswdgen.py
+++ pswdgen.py
@@ -1,14 +1,29 @@
 import random
+import tkinter as tk
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
-symbols = "[]}{()*:/,._-"
 
-all = lower + upper + numbers + symbols
+pwd = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]}{()*:/,._-"
 
-length = 16
+length = 32
 
-password = "".join(random.sample(all, length))
 
-print(password)
+def genpass():
+    password = "".join(random.sample(pwd, length))
+    label.config(text = password)
+
+
+window = tk.Tk()
+
+label = tk.Label(text = "click to generate password", width = 36,height = 2)
+
+button = tk.Button(
+    text = "Generate",
+    width = 15,
+    height = 2,
+    command = genpass
+)
+
+label.pack()
+button.pack(pady = 5)
+
+window.mainloop()
